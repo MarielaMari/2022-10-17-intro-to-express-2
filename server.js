f1360b84a1f3e3c4d5987720ab30d94f42163243
Mariela <mvmihilova@gmail.com>
2022-10-23 06:08:03 -0600

    post hello

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express ();
 const port = 4004;
 
-app.listen (4004, () => {
+app.post ('/hello', (request, response) => {
+  response.send ('Hello world!');
+});
+
+
+app.listen (port, () => {
   console.log (`Web server running on port ${port}`);
 });
